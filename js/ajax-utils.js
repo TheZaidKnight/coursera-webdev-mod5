acfffa1011dff4eea200c1f64a9b74a1f2181d79
TheZaidKnight <zaidmasood144@gmail.com>
2024-01-29 18:37:31 +0500

    eleventh commit

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -33,21 +33,6 @@ ajaxUtils.sendGetRequest =
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
   };
-// Define the responseHandler function
-//function responseHandler(data) {
-  // Check if data is an array (you might need to adjust this based on your server response)
-  //if (Array.isArray(data)) {
-    // Iterate over the array and log each element to the console
-    //data.forEach(function (item) {
-      //console.log(item);
-    //});
-  //} else {
-    // Log the entire data object to the console
-    //console.log(data);
-  //}
-//}
-
-
 
 
 // Only calls user provided 'responseHandler'
@@ -76,8 +61,6 @@ function handleResponse(request,
 
 // Expose utility to the global object
 global.$ajaxUtils = ajaxUtils;
-// Make an AJAX GET request using the utility
-//$ajaxUtils.sendGetRequest('your_api_endpoint', responseHandler());
 
 
 })(window);
